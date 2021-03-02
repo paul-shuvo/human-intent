eb2bf2e879026ebe598e9e0d374265c8c110b8e9
paul-shuvo <o.k.paul@gmail.com>
2021-03-02 12:51:27 -0800

    added patterns for verbal instructions

--- src/arm_pose/src/verbal_instruction.py
+++ src/arm_pose/src/verbal_instruction.py
@@ -3,6 +3,16 @@ import rospy
 from std_msgs.msg import String
 import speech_recognition as sr
 
+import spacy
+from spacy.matcher import Matcher
+nlp = spacy.load('en_core_web_sm')
+nlp.Defaults.stop_words -= {'give', 'Give', 'put', 'Put',
+                            'this', 'This', 'that', 'That',
+                            'here', 'Here', 'there', 'There'}
+
+
+matcher = Matcher(nlp.vocab)
+
 # r = sr.Recognizer()
 # mic = sr.Microphone(device_index=9)
 # with mic as source:
@@ -53,13 +63,55 @@ class VerbalInstruction:
             # next iteration.
             try:
                 self.instruction_msg = self.speech_recognizer.recognize_google(audio)
+                if self.instruction_msg in ["exit", "quit"]:
+                    rospy.signal_shutdown("User command")
             except sr.UnknownValueError:
                 rospy.loginfo("Speaker is quiet")
                 continue
             rospy.loginfo(self.instruction_msg)
+            self.get_object_info(self.instruction_msg)
             self.verbal_instruction_pub.publish(self.instruction_msg)
             rate.sleep()
 
+    def get_object_info(self, instruction_msg):
+        matcher.add("action", None, [{"POS": "VERB"},
+                                     {"POS": "PRON", "OP": "*"},
+                                     {},
+                                     {"POS": "ADJ", "OP": "*"},
+                                     {"POS": "NOUN"}])
+
+        matcher.add("attr", None, [{"POS": "ADJ"}])
+        matcher.add("pos", None, [{"LEMMA": {"IN": ["right", "left", "front", "back"]}}])
+
+        doc = nlp(instruction_msg)
+        matches = matcher(doc)
+        object_info = {}
+        object_name = ""
+        action = ""
+        attr = []
+        pos = ""
+
+        for match_id, start, end in matches:
+            string_id = nlp.vocab.strings[match_id]
+            print(string_id)
+            if string_id == "action":
+                object_name = doc[end-1]
+                action = doc[start]
+
+            if string_id == "attr":
+                attr.append(doc[start])
+
+            if string_id == "pos":
+                pos = doc[start]
+            # span = doc[start:end]  # The matched span
+            # print(string_id, start, end, span.text)
+
+        object_info[object_name] = {}
+        object_info[object_name]["action"] = action
+        object_info[object_name]["attr"] = attr
+        object_info[object_name]["pos"] = pos
+        print(object_info)
+
 
 if __name__ == "__main__":
-    VerbalInstruction(device_index=9)
+    VerbalInstruction(device_index=2)
